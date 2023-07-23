516c6d77ea63bf6831d79f74f0ec56471d81b18d
kegdeg <131921850+kegdeg@users.noreply.github.com>
2023-07-22 21:53:13 -0600

    Update k-means.sh

--- k-means.sh
+++ k-means.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+mahout canopy \
+ -i docw-vectors-manhattan/tfidf-vectors \
+ -ow \
+ -o docw-vectors-manhattan/docs-canopy-centroids-Manhattan \
+ -dm org.apache.mahout.common.distance.ManhattanDistanceMeasure \
+ -t1 0.5 \
+ -t2 0.3
+
 for k in {2..10}
 do
   echo "Running kmeans with k = $k"
