78d35599c7caf917fee2a26ca54a5699561fcfb1
kegdeg <131921850+kegdeg@users.noreply.github.com>
2023-07-22 22:37:18 -0600

    Update k-means.sh

--- k-means.sh
+++ k-means.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-mahout canopy \
- -i docw-vectors-manhattan/tfidf-vectors \
- -ow \
- -o docw-vectors-manhattan/docs-canopy-centroids-Manhattan \
- -dm org.apache.mahout.common.distance.ManhattanDistanceMeasure \
- -t1 0.5 \
- -t2 0.3
+#mahout canopy \
+# -i docw-vectors/tfidf-vectors \
+# -ow \
+## -o docw-vectors/docs-canopy-centroids-Manhattan \
+# -dm org.apache.mahout.common.distance.ManhattanDistanceMeasure \
+# -t1 0.5 \
+# -t2 0.3
 
-for k in {2..10}
-do
-  echo "Running kmeans with k = $k"
+
+k_values=(3 4 5 6 7 8 9 10)
+
+for k in "${k_values[@]}"; do
+  echo "Running kmeans with k = ${k}"
   mahout kmeans \
-   -i docw-vectors-manhattan/tfidf-vectors \
+   -i docw-vectors/tfidf-vectors \
    -c docw-canopy-centroids-Manhattan \
-   -o docs-kmeans-clusters-Manhattan-$k \
+   -o hdfs://lena/user/nmart001/docs-kmeans-clusters-Manhattan-${k} \
    -dm org.apache.mahout.common.distance.ManhattanDistanceMeasure \
    -cl \
    -cd 0.1 \
    -ow \
    -x 20 \
-   -k $k
+   -k ${k}
 done
